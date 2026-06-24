f4352132b50edc0334d27474feff20fe61d51597
moranmoriarty5 <moranmoriarty22@gmail.com>
2026-06-24 14:29:52 +0700

    transisi ke mysql

--- src/config/database.js
+++ src/config/database.js
@@ -8,7 +8,8 @@ export const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
-    dialect: process.env.DB_DIALECT || "postgres",
+    port: process.env.DB_PORT, // || 5432,
+    dialect: process.env.DB_DIALECT || "mysql",
+    // dialect: process.env.DB_DIALECT || "postgres",
   }
 );
